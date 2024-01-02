e3e789a87deec64dba0823bfe00396b98ab69b2a
Sherif Awad <sherif.hero1@gmail.com>
2024-01-02 21:55:27 +0200

    task 0 pos 0 neg

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+#chack if negativ
+if (number < 0):
+    print(f"{number} is negative")
+#check if zero
+elif (number == 0):
+    print(f"{number} is zero")
+#chak if postive
+elif (number > 0):
+    print(f"{number} is positive")
