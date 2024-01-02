1c01d513ade34543d34fc12f8861a8aaae648386
Sherif Awad <sherif.hero1@gmail.com>
2024-01-02 23:55:10 +0200

    c task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,37 @@
+/*
+ * File: insert_number
+ *
+ * Author: Sherif Awad
+ */
+
+#include "lists.h"
+
+/**
+ * insert_node - inserts num to singl list.
+ * @head: pointer ot head
+ * @number: number
+ * Return: pointer to new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *n = *h, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (n == NULL || n->n >= number)
+	{
+		new->next = n;
+		*h = new;
+		return (new);
+	}
+
+	while (n && n->next && n->next->n < number)
+		n = n->next;
+	new->next = n->next;
+	n->next = new;
+
+	return (new);
+}
