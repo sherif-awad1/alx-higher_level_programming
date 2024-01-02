ee390bf3414fde0cf9af703db06922b13a9f018e
Sherif Awad <sherif.hero1@gmail.com>
2024-01-03 00:11:24 +0200

    magic calc

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''
+File: 102-magic_calculation.py
+
+Author: Sherif Awad
+'''
+
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
