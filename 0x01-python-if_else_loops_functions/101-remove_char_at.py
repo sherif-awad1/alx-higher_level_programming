939f05429865fb938b95583bdd87db8739ba2f62
Sherif Awad <sherif.hero1@gmail.com>
2024-01-03 00:09:28 +0200

    remove

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''
+File: 101-remove-char at.by
+
+Author: Sherif Awad
+'''
+
+
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
