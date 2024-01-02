1d2d0ef70765ecd1409d78451409022cefc3aae1
Sherif Awad <sherif.hero1@gmail.com>
2024-01-02 22:44:10 +0200

    numbere

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''
+File: 5-print_comb2.py
+Author: Sherif Awad
+'''
+for num in range(0, 100):
+    if num == 99:
+        print("{}".format(num))
+    else:
+        print("{:02}".format(num), end=", ")
