1a4b5d724c282a92533173b4ca308f45f063b7e2
Sherif Awad <sherif.hero1@gmail.com>
2024-01-03 00:06:15 +0200

    alpharev

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''
+File: 100-print_tevapl.py
+
+Author: Sherif Awad
+'''
+
+
+i = 0
+for ch in range(ord('z'), ord('a') - 1, -1):
+    print('{}'.format(chr(ch - i)), end="")
+    i = 32 if i == 0 else 0
+
