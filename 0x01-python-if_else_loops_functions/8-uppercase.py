d0f5da9ec5dd1e165bc35933be504a512263f2aa
Sherif Awad <sherif.hero1@gmail.com>
2024-01-02 23:05:48 +0200

    upper cast

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''
+File:8-uppercase.pu
+Authpe: Sherif Awad
+'''
+
+
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+                c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
