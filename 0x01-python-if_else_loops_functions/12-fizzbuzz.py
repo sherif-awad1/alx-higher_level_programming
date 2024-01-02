9452bb54121c12e3a452d6eb34a25b15907d96a4
Sherif Awad <sherif.hero1@gmail.com>
2024-01-02 23:23:38 +0200

    fizbuz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''
+File: 12-fizzbuzz.py
+
+Author: Sherif Awad
+'''
+
+
+def fizzbuzz():
+    if num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif num % 3 == 0:
+            print("Fizz ", end="")
+        elif num % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
