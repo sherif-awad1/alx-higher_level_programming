e84f62d65b88c2ae268202c46d5e199acb9c0801
Sherif Awad <sherif.hero1@gmail.com>
2024-01-04 18:57:32 +0200

    check

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     ''' print all in hidden file '''
     import hidden_4
 
-    nms = dir(hidden_4)
-    for name in nms:
-        if name[:2] != "__":
-            print(name)
+    for n in dir(hidden_4):
+        if not n.startswith("__"):
+            print("{:s}".format(n))
 
