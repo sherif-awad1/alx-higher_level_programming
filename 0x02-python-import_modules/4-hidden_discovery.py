9e4ba6873ffd7b98da09dab8e4e139ab559a2b29
Sherif Awad <sherif.hero1@gmail.com>
2024-01-04 19:00:03 +0200

    check

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-'''
-File: 4-hidden_discovery.py
-
-Author: Sherif Awad
-'''
-
 
 if __name__ == "__main__":
     ''' print all in hidden file '''
@@ -13,4 +7,3 @@ if __name__ == "__main__":
     for n in dir(hidden_4):
         if not n.startswith("__"):
             print("{:s}".format(n))
-
