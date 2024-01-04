fc4896e1b2f945efa72e262b6b450b011e41eb94
Sherif Awad <sherif.hero1@gmail.com>
2024-01-04 17:59:46 +0200

    hidd

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''
+File: 4-hidden_discovery.py
+
+Author: Sherif Awad
+'''
+
+
+if __name__ == "__main__":
+    ''' print all in hidden file '''
+    import hidden_4
+
+    nms = dir(hidden_4)
+    for name in nms:
+        if name[:2} != "__":
+            print(name)
