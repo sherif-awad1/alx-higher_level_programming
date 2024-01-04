368af71e78bf99d3c485733523dd0b5efd7f042a
Sherif Awad <sherif.hero1@gmail.com>
2024-01-04 16:32:17 +0200

    test task 1

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+File:0-add.py
+
+Author: Sherif Awad
+'''
+
+
+if __name__ == "__main__":
+    ''''
+    print the sume 1 + 2
+    '''
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
