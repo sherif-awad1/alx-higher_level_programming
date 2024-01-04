03443a1fa8abb5fa301b67d88bea26bd44f53fb3
Sherif Awad <sherif.hero1@gmail.com>
2024-01-04 19:19:48 +0200

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+'''
+File: 100-my_caculator.py
+
+Author: Sherif Awad
+'''
+
+
+if __name__ == "__main__":
+    '''handle 3arg calalation '''
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    leng = len(sys.argv) - 1
+    if leng != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    op = {"+": add. "-":sub, "*":mul, "/":div}
+    if sys.argv[2] not in list(op.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, op[sys.argv[2]](a, b)))
