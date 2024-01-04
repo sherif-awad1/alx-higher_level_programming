ea6c64f6c6352f361ebe24e15a685ab654416d11
Sherif Awad <sherif.hero1@gmail.com>
2024-01-04 17:42:16 +0200

    count all arg

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''
+File: 3-infinite_add.py
+
+Author: Sherif Awad
+'''
+
+
+if __name__ == "__main__":
+    ''' print sum of all argment '''
+    import sys
+
+    count = 0
+    leng = len(sys.argv) - 1
+    for n in range(leng):
+        count += int(sys.argv[n + 1])
+    print("{}".format(count))
