5f933840907246d3e98ab32495bb28e05e913a35
Sherif Awad <sherif.hero1@gmail.com>
2024-01-04 17:32:57 +0200

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''
+File: 2-args.py
+
+Author: Sherif Awad
+'''
+
+
+if __name__ == "__main__":
+    ''' print count argments list'''
+    import sys
+
+    leng = len(sys.argv) - 1
+    if leng == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments".format(leng))
+    for n in range(leng):
+        print("{}: {}".format(n + 1, sys.argv[n + 1]))
