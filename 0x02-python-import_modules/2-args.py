99643649ea18f9f8085dbb74e1bc74fcecc4ba8c
Sherif Awad <sherif.hero1@gmail.com>
2024-01-04 17:35:55 +0200

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
     leng = len(sys.argv) - 1
     if leng == 1:
         print("1 argument:")
+    elif leng == 0:
+        print("0 arguments.")
     else:
-        print("{} arguments".format(leng))
+        print("{} arguments:".format(leng))
     for n in range(leng):
         print("{}: {}".format(n + 1, sys.argv[n + 1]))
