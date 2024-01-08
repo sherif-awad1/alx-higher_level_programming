1564b33b2b61ddd1d0aa0643fe3019e25359f5d4
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 19:22:51 +0200

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''
+File: 4-new_in_list.py
+Author: Sherif Awad
+'''
+
+
+def new_in_list(my_list, idx, element):
+    ''' '''
+    copy = my_list.copy()
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list.copy()
+    else:
+        copy[idx] = element
+        return copy
