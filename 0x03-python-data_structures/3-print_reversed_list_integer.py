f90c43215f2727c609aa1986ae5e47be6d7f2153
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 19:16:30 +0200

    revers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''
+File: 3-print_reversed_list_integer.py
+Author: Sherif Awad
+'''
+
+
+def print_reversed_list_integer(my_list=[]):
+    '''prints all integers of a list, in reverse orde '''
+    if my_list:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
