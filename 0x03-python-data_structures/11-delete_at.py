2e305dd02bf2faa41c10d00789e44f5817fe29af
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 20:15:17 +0200

    task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''
+File: 11-delete_at.py
+Author: Sherif awad
+'''
+
+
+def delete_at(my_list=[], idx=0):
+    ''' function that deletes item at specific position in list '''
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        del my_list[idx]
+    return my_list
