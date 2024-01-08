5e17934860b13ea13e441f80d1ee46f5525b9e7b
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 20:01:11 +0200

    last tuch 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''
+File: 8-multiple_returns.py
+Author: Sherif Awad
+'''
+
+
+def multiple_returns(sentence):
+    '''function that returns a tuple with the length of a string'''
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
