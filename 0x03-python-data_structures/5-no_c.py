fe40cbe5aabd99e51093f56fae07b9eea5ce7d61
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 19:37:25 +0200

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+'''
+File: 5-no_c.py
+Author: Sherif Awad
+'''
+
+
+def no_c(my_string):
+    '''removes all characters c and C from a string'''
+    new = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(new))
