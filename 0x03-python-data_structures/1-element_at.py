e91aa5b9975811f5df6ed451347d6b64842410f8
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 18:57:45 +0200

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''
+File:1-element_at.py
+Author: Sherif Awad
+'''
+
+
+def element_at(my_list, idx):
+    '''retrieves an element from a list like in C '''
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    else:
+        return (my_list[idx])
