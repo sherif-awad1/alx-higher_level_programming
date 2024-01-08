e7060f21aaaba74356c0c7a549eec9703857141a
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 19:24:15 +0200

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,7 @@ Author: Sherif Awad
 
 
 def new_in_list(my_list, idx, element):
-    ''' '''
+    '''  replaces element in list at position '''
     copy = my_list.copy()
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list.copy()
