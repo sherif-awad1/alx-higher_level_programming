5f0518213ac77ee465c0896f9dce18b50397375b
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 20:05:32 +0200

    mex or max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''
+File: 9-max_integer.py
+Author: Sherif Awad
+'''
+
+
+def max_integer(my_list=[]):
+    ''' '''
+    if len(my_list) == 0:
+        return (None)
+
+    mex = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > mex:
+            mex = my_list[i]
+    return (mex)
