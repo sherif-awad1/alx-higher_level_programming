09880923aee23ad8f6323045b166b3f07d8fb5c4
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 18:31:08 +0200

    first

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+File:0-print_list_integer.py
+Author: Sherif Awad
+'''
+
+def print_list_integer(my_list=[]):
+    ''' print integer listtt '''
+    for i in range(my_list):
+        print("{:d}".format(my_list[i]))
