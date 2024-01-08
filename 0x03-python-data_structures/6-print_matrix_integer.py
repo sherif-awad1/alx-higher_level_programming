be0add4883e3e75cab52947be74aa344c2c21b0c
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 19:50:59 +0200

    test 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''
+File:6-print_matrix_integer.py
+Author: Sherif Awad
+'''
+
+
+def print_matrix_integer(matrix=[[]]):
+    ''' function that prints a matrix of integers. '''
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+            print("")
