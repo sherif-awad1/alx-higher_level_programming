3aeec368b29fd0077395f00e37514c82d75d96c2
Sherif Awad <sherif.hero1@gmail.com>
2024-01-08 19:10:00 +0200

    after codestyel

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,6 +7,6 @@ Author: Sherif Awad
 
 def replace_in_list(my_list, idx, element):
     ''' replaces an element of a list at a specific position (like in C) '''
-    if idx >= 0 and idx < (len(my_list) - 1):
+    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
     return (my_list)
