d633929644d0f5b7d0ce517d665c569872d17b41
Sherif Awad <sherif.hero1@gmail.com>
2024-01-10 21:06:57 +0200

    test 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-"""
-File: 11-multiply_list_map.py
-
-Author: Sherif Awad
-"""
-
-
 def multiply_list_map(my_list=[], number=0):
     return (list(map((lambda c: c*number), my_list)))
