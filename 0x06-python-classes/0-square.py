0dc6e91e36c68b1ad8bff7c745c8a5bff14e23d3
Sherif Awad <sherif.hero1@gmail.com>
2024-02-20 16:03:52 +0200

    old task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Autho: Sherif Awad
+"""def class Square"""
+
+
+class Square:
+    """represent square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Author: Sherif Awad
+
+"""define class square"""
+
+
+class Square:
+    """represent  square"""
+
+    def __init__(self, size):
+        self.__size = size
